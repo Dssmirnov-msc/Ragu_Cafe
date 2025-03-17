3ef265d892f280dcae4dd7b50856c41574057e81
Ylmangust <adeli-2019@mail.ru>
2025-03-17 15:11:27 +0300

    1st commit

---
+++ Ragu_Cafe/src/main/java/mephi/b22901/toriregi/ragu_cafe/Ragu_Cafe.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mephi.b22901.toriregi.ragu_cafe;
+
+/**
+ *
+ * @author Регина
+ */
+public class Ragu_Cafe {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
